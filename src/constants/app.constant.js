4600d947168eeb389eefc29a7d8d7bd59c677b8c
IAMHUDA <hudamiftahul362@gmail.com>
2025-08-29 14:10:19 +0700

    mengubah nama title menjadi sigapan

--- src/constants/app.constant.js
+++ src/constants/app.constant.js
@@ -1,5 +1,5 @@
-export const APP_NAME = "Tailux";
-export const APP_KEY = "tailux";
+export const APP_NAME = "Sigapan";
+export const APP_KEY = "sigapan";
 
 // Redirect Paths
 export const REDIRECT_URL_KEY = "redirect";
